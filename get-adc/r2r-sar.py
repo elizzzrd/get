5d1d218ced3d5978357c05a55e23d28aaeed3318
Elizaveta Gardina <lizigard@gmail.com>
2026-05-07 11:05:04 +0300

    [MODIFIED] divided files with class definition and usage of sc and sar

--- get-adc/r2r-sar.py
+++ get-adc/r2r-sar.py
@@ -6,24 +6,21 @@ from r2r_adc import R2R_ADC
 
 
 #======================================================================
-# ADC SC (последовательного счета)
+# SAR ADC — Successive Approximation Register ADC
 
 voltage_values = []
 time_values = []
 duration = 3.0
-dynamic_range = 3.3
-
-
-adc = R2R_ADC(dynamic_range, compare_time=0.0001)
+adc = R2R_ADC(3.300)
 
 try:
-    start_time = time.time_ns()     # return nanoseconds
+    start_time = time.time_ns()
     time_0 = start_time
 
-    while (((time_0 - start_time) / 1e9) < duration):
+    while ((time_0 - start_time) / 1e9 < duration):
         time_0 = time.time_ns()
-        volt = adc.get_sc_voltage()
-
+        volt = adc.get_sar_voltage()
+        
         print(volt, (time_0 - start_time) / 1e9)
 
         time_values.append((time_0 - start_time) / 1e9)
@@ -31,7 +28,6 @@ try:
 
     adcplot.plot_voltage_vs_time(time_values, voltage_values, adc.dynamic_range)
     adcplot.plot_sampling_period_hist(time_values)
-
-
+        
 finally:
     adc.deinit()
